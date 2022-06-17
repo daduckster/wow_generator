f42c054207d4b23b3b04defa3c384bd7111389f5
daduckster <vlasak.o@yandex.ru>
2022-06-17 22:18:23 +0200

    replaced images with webp format

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -5,10 +5,10 @@ function Hero() {
   return (
     <div className={styles.heroContainer}>
       <figure className={styles.wavesContainer}>
-        <img className={styles.img} src="./assets/waves.png" alt="waves pattern" />
+        <img className={styles.img} src="./assets/waves.webp" alt="waves pattern" />
       </figure>
       <figure className={styles.palmsContainer}>
-        <img className={styles.img} src="./assets/palms.png" alt="roads with palms" />
+        <img className={styles.img} src="./assets/palms.webp" alt="roads with palms" />
       </figure>
     </div>
   );
